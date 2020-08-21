d80808488a168af79c59fa5dda3502980c672401
grandmastar27 <grandmastar27@gmail.com>
2020-08-21 19:50:00 +0200

    Create main.swift

---
+++ Swift-Types/main.swift
@@ -0,0 +1,17 @@
+import Swift; //Swift package
+
+//Official documentation: https://docs.swift.org/swift-book/ReferenceManual/Types.html
+
+//declaring variable syntax in swift
+//variable name:type = expression
+
+var myAge:Int = 20; //Int typed variable with value 20
+var myInt8:Int8 = 127; //Int8 typed variable with value 127
+var myUInt8:UInt8 = 255; //UInt8 typed variable with value 255
+var myInt64:Int64 = 9223372036854775807; //Int64 typed variable with value 9223372036854775807
+var myUInt64:UInt64 = 18446744073709551615; //UInt64 typed variable with value 18446744073709551615
+var myGradeAverages:Double = 4.75; //Double typed variable with value 4.75
+var myGradeAveragesInFloat:Float = 4.75; //Float typed variable with value 4.75
+var myName: String = "Bob"; //String typed variable with value Bob
+var myCharacter:Character = "B"; //Character typed variable with value B
+var amIHungry:Bool = true; //Boolean typed variable with value true
